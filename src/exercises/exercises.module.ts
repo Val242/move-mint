6e62a2e84ec96b46eed1af3051d8b6c78eea1514
=Val242 <=ebongvalentine70@gmail.com>
2026-03-23 07:38:45 +0100

    Adjusted the prisma model

--- src/exercises/exercises.module.ts
+++ src/exercises/exercises.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExercisesService } from './exercises.service';
 import { ExercisesController } from './exercises.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ExercisesController],
   providers: [ExercisesService],
 })
